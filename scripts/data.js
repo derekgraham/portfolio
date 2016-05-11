47f1affa65bfcaf60ae7431ae4801cea7fa6c621
Derek Graham <derekgraham@mac.com>
2016-05-10 21:52:49 -0700

    content array database, tohtml method, sort by date, video or image content, formatting

---
+++ scripts/data.js
@@ -0,0 +1,79 @@
+// projectName
+// projectDate
+// projectImage
+// projectTitle
+// projectDescription
+
+
+var myPortfolioData = [ {
+	name: 'Project One',
+	maker: 'Derek Graham',
+	category: 'digital',
+	publishedDate: '2011-01-01',
+	mediaType: 'image',
+	mediaURL: 'images/fishing.jpg',
+	title: '',
+	description: '',
+	projectURL: '',
+	technologies: ''
+}, {
+	name: 'Fireplace',
+	maker: 'Derek Graham',
+	category: 'digital',
+	publishedDate: '2010-01-01',
+	mediaType: 'image',
+	mediaURL: 'images/fireplace.jpg',
+	title: '',
+	description: '',
+	projectURL: '',
+	technologies: ''
+},{
+	name: 'OnTrack - Health Tracking Made Easy!',
+	maker: 'Derek Graham',
+	category: 'digital',
+	publishedDate: '2016-05-06',
+	mediaType: 'image',
+	mediaURL: 'images/ontrack.jpg',
+	title: 'Codefellows Code 201 final project',
+	description: 'Web application to help patients undergoing chemo-therapy to create daily health goals and easily input activities and events to track daily progress towards their goals and chart their data history.',
+	projectURL: 'https://luminous-torch-2017.firebaseapp.com/',
+	technologies: 'HTML5, CSS3, JavaScript, Chart.js, Firebase'
+},{
+	name: 'Interbay Studio Collective',
+	maker: 'Derek Graham',
+	category: 'analog',
+	publishedDate: '2012-01-01',
+	mediaType: 'image',
+	mediaURL: 'images/studio.jpg',
+	title: '',
+	description: 'Unique property in the Interbay Industrial area neighborhood with a caretakers\' apartment and 5 workspaces',
+	projectURL: 'http://www.interbaystudiocollective.com',
+	technologies: 'Aluminum stairs, metal siding & roofing, hydronic radiant heat, upcycled vintage school doors, tenant kitchenette'
+},{
+	name: 'Coop 401',
+	maker: 'Derek Graham',
+	category: 'analog',
+	publishedDate: '2015-08-01',
+	mediaType: 'image',
+	mediaURL: 'images/coop.jpg',
+	title: '',
+	description: 'Upgraded chicken coop for 20 hens',
+	projectURL: '',
+	technologies: 'Metal siding, standing seam roof, sweep-out'
+},{
+	name: 'Studio Blinkin\' lights',
+	maker: 'Derek Graham',
+	category: 'hybrid',
+	publishedDate: '2015-12-01',
+	mediaType: 'video',
+	mediaURL: {src: "video/IMG_6194.m4v", type:"video/mp4"},
+	title: '',
+	description: 'Dynamic Light Sculpture',
+	projectURL: '',
+	technologies: 'Arduino, DMX, RGB LEDs'
+}
+
+
+
+
+]

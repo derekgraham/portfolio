dc0c30fc2927841cbcbda385e05264303211bcdb
Derek Graham <derekgraham@mac.com>
2016-05-14 09:23:32 -0700

    added handlebar template details for time since created, pub date as title tool tip

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -6,13 +6,14 @@ function Projects (projectObject) {
 }
 
 timeSinceCreated = function(createdDate){
+  var out = 'about ';
   myDays = parseInt((new Date() - new Date(createdDate))/60/60/24/1000);
 
   if (myDays < 365) {
-    return myDays + ' days ago';
+    out += myDays + ' days ago';
  		
-  } else return parseInt(myDays / 365) + ' years ago';
-
+  } else out += parseInt(myDays / 365) + ' years ago';
+  return out;
 };
 
 Projects.prototype.generateHtml = function (){
@@ -24,57 +25,12 @@ Projects.prototype.generateHtml = function (){
       return options.inverse(this);
     }
   });
-
-
+  this.relativePubDate = timeSinceCreated(this.publishedDate);
   var $source = $('#project-template').html();
   var filter = Handlebars.compile($source);
   $('#project-list').append(filter(this));
 };
 
-Projects.prototype.toHtml = function() {
-  var $tempProject = $('article.template').clone();
-
-  $tempProject.attr('data-category', this.category);
-  $tempProject.find('h5').html(this.name);
-  $tempProject.find('address a').html(this.maker);
-
-
-  switch (this.mediaType ){
-  case 'image':
-    $tempProject.find('img').attr('src',this.mediaURL);
-    $tempProject.find('.project-image-link').attr('href',this.projectURL);
-    $tempProject.find('.project-image-title').html(this.title);
-    $tempProject.find('.project-image').toggleClass('hidden');
-    break;
-  case 'video':
-    console.log($tempProject.find('video source'));
-    $tempProject.find('video source').attr('src',this.mediaURL['src']);
-    $tempProject.find('video source').attr('type',this.mediaURL['type']);
-    $tempProject.find('.project-video').toggleClass('hidden');
-    break;
-  case 'gallery':
-    break;
-  }
-
-//   $tempProject.find('address a').attr('href',this.authorUrl);
-  $tempProject.find('.project-body').html(this.description);
-  $tempProject.find('time').attr('pubdate',this.publishedDate);
-
-// // console.log($tempProject);
-
-//   // This is a separate inclusion of the publication date as a 'title' attribute
-//   // to show on hover:
-  $tempProject.find('time[pubdate]').attr('title', this.publishedDate);
-
-//   // Display the date as a relative number of "days ago":
-  $tempProject.find('time').html('about ' + timeSinceCreated(this.publishedDate));
-
-  // $tempProject.append('<hr>');
-
-  $tempProject.removeClass('template');
-
-  return $tempProject;
-};
 
 // Sort our data by date published, descending order
 myPortfolioData.sort(function(a,b) {

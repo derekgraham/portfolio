19731c34c65bbb412b86df356761de40656de2f7
Derek Graham <derekgraham@mac.com>
2016-05-16 10:47:59 -0700

    updated relative date/time generation to include months

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -6,12 +6,21 @@ function Projects (projectObject) {
 }
 
 timeSinceCreated = function(createdDate){
+  var months = ['eleven','one','two','three','four','five','six','seven','eight','nine','ten', 'eleven'];
   var out = 'about ';
   myDays = parseInt((new Date() - new Date(createdDate))/60/60/24/1000);
-
+  // myMonths = Math.abs(parseInt(( new Date().getMonth() - new Date(createdDate).getMonth
+  //fixme: do actual months from now calculation instead of 30 days.
   if (myDays < 365) {
-    out += myDays + ' days ago';
- 		
+    myMonths = parseInt(( new Date(createdDate).getMonth() - new Date().getMonth()));
+    console.log(createdDate + ' ' + myMonths + ' ' + ( (myMonths < 0) ? 'here ' + (12-myMonths) % 12: 'not ' + Math.abs(12-myMonths % 12 -1)));
+    console.log(createdDate + months[myMonths < 0 ? (12-myMonths) % 12 : Math.abs(12-myMonths % 12 -1)]);
+    if (myDays < 30){ 
+      out += myDays + ' days ago';
+    } else {
+   
+      out += months[myMonths < 0 ? (12-myMonths) % 12 : Math.abs(12-myMonths % 12 -1)] + ' months ago';
+    }
   } else out += parseInt(myDays / 365) + ' years ago';
   return out;
 };
@@ -25,6 +34,7 @@ Projects.prototype.generateHtml = function (){
       return options.inverse(this);
     }
   });
+
   this.relativePubDate = timeSinceCreated(this.publishedDate);
   var $source = $('#project-template').html();
   var filter = Handlebars.compile($source);

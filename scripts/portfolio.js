5f3c8b13be0090dd21d37e56433f7cb9b5619a32
Derek Graham <derekgraham@mac.com>
2016-05-16 22:01:24 -0700

    updated to get data from json or local storage

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,31 +1,27 @@
-var projects = [];
-
-function Projects (projectObject) {
+function Project (projectObject) {
   for(var prop in projectObject) {this[prop]= projectObject[prop];}
 
 }
 
+Project.all = [];
+ 
 timeSinceCreated = function(createdDate){
   var months = ['eleven','one','two','three','four','five','six','seven','eight','nine','ten', 'eleven'];
   var out = 'about ';
   myDays = parseInt((new Date() - new Date(createdDate))/60/60/24/1000);
-  // myMonths = Math.abs(parseInt(( new Date().getMonth() - new Date(createdDate).getMonth
   //fixme: do actual months from now calculation instead of 30 days.
   if (myDays < 365) {
     myMonths = parseInt(( new Date(createdDate).getMonth() - new Date().getMonth()));
-    console.log(createdDate + ' ' + myMonths + ' ' + ( (myMonths < 0) ? 'here ' + (12-myMonths) % 12: 'not ' + Math.abs(12-myMonths % 12 -1)));
-    console.log(createdDate + months[myMonths < 0 ? (12-myMonths) % 12 : Math.abs(12-myMonths % 12 -1)]);
     if (myDays < 30){ 
       out += myDays + ' days ago';
     } else {
-   
-      out += months[myMonths < 0 ? (12-myMonths) % 12 : Math.abs(12-myMonths % 12 -1)] + ' months ago';
+      out += months[myMonths < 0 ? (12-myMonths) % 12 : Math.abs(12-myMonths % 12 - 1)] + ' months ago';
     }
   } else out += parseInt(myDays / 365) + ' years ago';
   return out;
 };
 
-Projects.prototype.generateHtml = function (){
+Project.prototype.generateHtml = function (){
 
   Handlebars.registerHelper('if', function(conditional, options) {
     if(conditional) {
@@ -41,20 +37,49 @@ Projects.prototype.generateHtml = function (){
   $('#project-list').append(filter(this));
 };
 
+Project.fillProjectArray = function (rawProjectData) {  
+  rawProjectData.sort(function(a,b) {
+    return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
+  });
 
-// Sort our data by date published, descending order
-myPortfolioData.sort(function(a,b) {
-  return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
-});
+  rawProjectData.forEach(function(ele) {
+    Project.all.push(new Project(ele));
+  });
 
-// Now iterate through our transformed collection and instantiate a new Project instance
-myPortfolioData.forEach(function(ele) {
-  projects.push(new Projects(ele));
-});
+};
 
-// Append each Project in the project section
-projects.forEach(function(a){
-  // $('#projects').append(a.toHtml());
-  a.generateHtml();
-});
+Project.loadProjectData = function () {
+  if (localStorage.myPortfolioDigest && localStorage.myPortfolioData){
+    Project.fillProjectArray(JSON.parse(localStorage.myPortfolioData));
 
+  } else {
+    $.getJSON({
+      url: 'data/portfolio.json', 
+      success: function(data, status, xhr){
+        localStorage.myPortfolioData = JSON.stringify(data);
+        localStorage.myPortfolioDigest = xhr.getResponseHeader('eTag');
+        Project.fillProjectArray(data);
+      }
+
+    });
+  }
+  portfolioPage.initPage();
+  
+};
+
+Project.checkForNewRemoteData = function() {
+  $.ajax({
+    type: 'HEAD',
+    url: 'data/portfolio.json',
+    success: function(data, status, xhr){
+      var newDigest = xhr.getResponseHeader('eTag');
+      if (newDigest !== localStorage.myPortfolioDigest){
+        localStorage.clear('myPortfolioData');
+        localStorage.clear('myPortfolioDigest');
+
+      }
+      Project.loadProjectData();
+
+    }
+  });
+};

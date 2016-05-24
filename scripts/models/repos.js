eeddadd403fc591874533bd72b4448f6708811af
Derek Graham <derekgraham@mac.com>
2016-05-24 16:31:31 -0700

    added functionality to display github repos, cleaned up some Css

---
+++ scripts/models/repos.js
@@ -0,0 +1,35 @@
+(function(module) {
+  var repos = {};
+  var followers = {};
+
+  repos.all = [];
+  // followers.all = [];
+
+  repos.requestRepos = function(callback) {
+    /* DONE: How would you like to fetch your repos? Someone say AJAX!?
+       Don't forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/derekgraham/repos' +
+            '?sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + gitHubToken},
+      success: function(data) {
+        repos.all = data;
+        callback();
+      }
+    });
+  };
+
+  repos.with = function(attr) {
+    /* DONE: This Model method filters the full repos collection based
+        on a particular attribute. You could use this to filter all
+        repos that have a non-zero `forks_count`, `stargazers_count`,
+        or `watchers_count`. */
+    return repos.all.filter(function(repo) {
+      return repo[attr]; 
+    });
+  };
+
+  module.repos = repos;
+  // module.followers = followers;
+})(window);
---
+++ scripts/views/repoView.js
@@ -0,0 +1,24 @@
+(function(module) {
+  var repoView = {};
+
+  var ui = function() {
+    var $repos = $('#repos'); 
+    $repos.find('ul').empty();
+    $repos.show().siblings().hide();
+  };
+
+  var render = Handlebars.compile($('#repo-template').html());
+  // var renderFollowers = Handlebars.compile($('#followers-template').html());
+
+  repoView.index = function() {
+    ui();
+    $('#repos .repos').append(
+      repos.with('name').map(render)
+    );
+    // $('#about .followers').append(
+    //   followers.all.map(renderFollowers)
+    // );
+  };
+
+  module.repoView = repoView;
+})(window);
